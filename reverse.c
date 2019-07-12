29f429f5db8f019abd2ab3c3426ed4fa3ab4e60e
ec2-user <kirankumarclk@gmail.com>
2019-07-12 07:30:11 +0000

    modified file

--- reverse.c
+++ reverse.c
@@ -1,9 +1,10 @@
 #include  <stdio.h>
 #include <string.h>
-int main(void)
-{char *str="ForgetCode";
+void rev()
+{
+char *str="ForgetCode";
  printf("Reverse the String:");
  for(int i=(strlen(str)-1);i>=0;i--)
  { printf("%c",str[i]);
- } return 0;
+ }
 }
